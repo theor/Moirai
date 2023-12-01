3f5b8cf45203aafa4c5744176138551b1637e1c5
theor <theo.richart@gmail.com>
2023-12-01 11:13:42 -0500

    cleanup

--- MoiraiWebServer/Client/MoiraiWebClient/src/utils.tsx
+++ MoiraiWebServer/Client/MoiraiWebClient/src/utils.tsx
@@ -1,21 +1,21 @@
 ﻿import {Property} from "./types.ts";
 import * as React from "react";
 import {Chip, Tooltip} from "@mui/material";
-import {useNavigate, useParams, useSearchParams} from "react-router-dom";
+import {useSearchParams} from "react-router-dom";
 
 export function makeEntityLink(str: string, selectedEntity: Property<number>): React.JSX.Element[] {
     const rx = /(?:(?:<#(?<id>\d+)>(?<link>[^<]+)<\/>)|(?<text>[^<\n]+))/ig;
-    const text = [...str.matchAll(rx)].map((match: RegExpMatchArray, i) => {
+    return [...str.matchAll(rx)].map((match: RegExpMatchArray, i) => {
         if (!match?.groups)
             return <span key={i}>???</span>;
         if (match.groups["text"]) {
             return <span key={i}>{match.groups["text"]}</span>;
         } else {
             let id: number = Number(match.groups["id"]);
-            return <Tooltip title={"#"+id} key={i}>{makeEntityChip(id, match.groups["link"], selectedEntity)}</Tooltip>
+            return <Tooltip title={"#" + id}
+                            key={i}>{makeEntityChip(id, match.groups["link"], selectedEntity)}</Tooltip>
         }
-    })
-    return text;
+    });
 }
 export function makeEntityChip(id: number, label: string, selectedEntity: Property<number>) {
     return <Chip size="small" color="primary"
