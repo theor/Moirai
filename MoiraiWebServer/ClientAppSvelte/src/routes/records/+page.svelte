1bc22b4b2956c5a9d853c96fc8c0b3316b663229
theor <theo.richart@gmail.com>
2024-10-14 00:27:25 -0400

    tanstacktable

--- MoiraiWebServer/ClientAppSvelte/src/routes/records/+page.svelte
+++ MoiraiWebServer/ClientAppSvelte/src/routes/records/+page.svelte
@@ -1,37 +1,47 @@
 <script lang="ts">
   import { moiraiStore } from '$lib/connection';
-  import { createTable, Subscribe, Render, createRender } from 'svelte-headless-table';
-    import { derived } from 'svelte/store';
-    import MoiraiText from '../../components/MoiraiText.svelte';
-    import { selectedEntity } from '$lib/utils';
-import { page } from '$app/stores';
-    
-    let sel;
-    sel = selectedEntity($page);
-    $: sel = selectedEntity($page);
+  import { derived } from 'svelte/store';
+  import { createSvelteTable, flexRender, getCoreRowModel } from '@tanstack/svelte-table';
+  import type { ColumnDef, TableOptions } from '@tanstack/svelte-table'
+  import MoiraiText from '../../components/MoiraiText.svelte';
+  import { selectedEntity } from '$lib/utils';
+  import { page } from '$app/stores';
+    import type { Record } from '$lib/types';
 
+  let sel;
+  sel = selectedEntity($page);
+  $: sel = selectedEntity($page);
 
-  const recordStore = derived([moiraiStore, page], ([$moiraiStore]) => {
-    return $moiraiStore.records.map(r => ({...r, selected: Number(sel.get())}));
+  const columns: ColumnDef<Record>[] = [
+    {
+      header: 'Year2',
+      accessorKey: 'year'
+    },
+    {
+      header: 'Text',
+      accessorKey: 'text',
+    //   cell: (info) => {
+    //     return createRender(MoiraiText, { text: record.value, selected: Number(sel.get()) });
+    //   }
+    }
+  ];
+  const options = derived([moiraiStore, page], ([$moiraiStore]) => {
+    return {
+      getCoreRowModel: getCoreRowModel(),
+      columns,
+      data: $moiraiStore.records.map((r) => ({ ...r, selected: Number(sel.get()) }))
+    };
   });
 
-  $: {
-        console.warn("!!!", $page.url.searchParams)  
-    }
-  const table = createTable(recordStore);
-  const columns = table.createColumns([
-    table.column({
-      header: 'Year',
-      accessor: 'year',
-    }),
-    table.column({
-      header: 'Text',
-      accessor: 'text',cell: (record) => {
-        return createRender(MoiraiText, { text: record.value, selected: Number(sel.get()) });
-      }
-    }),
-  ]);
-  const { headerRows, rows, tableAttrs, tableBodyAttrs,  } = table.createViewModel(columns);
+//   const rerender = () => {
+//     options.update((options) => ({
+//       ...options,
+//       data: defaultData
+//     }));
+//   };
+
+  const table = createSvelteTable(options);
+//   const { headerRows, rows, tableAttrs, tableBodyAttrs } = table.createViewModel(columns);
 </script>
 
 <div class="m-4">
@@ -44,37 +54,56 @@ import { page } from '$app/stores';
     {$moiraiStore.records.length}
   </div>
   <div>
-    <table class="table table-hover table-compact table-auto w-full" {...$tableAttrs}>
-      <thead>
-        {#each $headerRows as headerRow (headerRow.id)}
-          <Subscribe rowAttrs={headerRow.attrs()} let:rowAttrs>
-            <tr {...rowAttrs}>
-              {#each headerRow.cells as cell (cell.id)}
-                <Subscribe attrs={cell.attrs()} let:attrs>
-                  <th {...attrs}>
-                    <Render of={cell.render()} />
+    <table class="table table-hover table-compact table-auto w-full" >
+        <thead>
+            {#each $table.getHeaderGroups() as headerGroup}
+              <tr>
+                {#each headerGroup.headers as header}
+                  <th>
+                    {#if !header.isPlaceholder}
+                      <svelte:component
+                        this={flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                      />
+                    {/if}
                   </th>
-                </Subscribe>
-              {/each}
-            </tr>
-          </Subscribe>
-        {/each}
-      </thead>
-      <tbody {...$tableBodyAttrs}>
-        {#each $rows as row (row.id)}
-          <Subscribe rowAttrs={row.attrs()} let:rowAttrs>
-            <tr {...rowAttrs}>
-              {#each row.cells as cell (cell.id)}
-                <Subscribe attrs={cell.attrs()} let:attrs>
-                  <td {...attrs}>
-                    <Render of={cell.render()} />
+                {/each}
+              </tr>
+            {/each}
+          </thead>
+          <tbody>
+            {#each $table.getRowModel().rows as row}
+              <tr>
+                {#each row.getVisibleCells() as cell}
+                  <td>
+                    <svelte:component
+                      this={flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    />
                   </td>
-                </Subscribe>
-              {/each}
-            </tr>
-          </Subscribe>
-        {/each}
-      </tbody>
+                {/each}
+              </tr>
+            {/each}
+          </tbody>
+          <tfoot>
+            {#each $table.getFooterGroups() as footerGroup}
+              <tr>
+                {#each footerGroup.headers as header}
+                  <th>
+                    {#if !header.isPlaceholder}
+                      <svelte:component
+                        this={flexRender(
+                          header.column.columnDef.footer,
+                          header.getContext()
+                        )}
+                      />
+                    {/if}
+                  </th>
+                {/each}
+              </tr>
+            {/each}
+          </tfoot>
     </table>
   </div>
 </div>
